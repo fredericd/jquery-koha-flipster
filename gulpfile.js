f3c254bea3c970a9924d432ce5aed205df104698
Frédéric Demians <f.demians@tamil.fr>
2016-05-02 17:52:56 +0200

    Tabs cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
 var gulp     = require('gulp'),
-	concat   = require('gulp-concat'),
-	rename   = require('gulp-rename'),
-	uglify   = require("gulp-uglify"),
-	cleancss = require('gulp-clean-css'),
-	jshint   = require('gulp-jshint');
+    concat   = require('gulp-concat'),
+    rename   = require('gulp-rename'),
+    uglify   = require("gulp-uglify"),
+    cleancss = require('gulp-clean-css'),
+    jshint   = require('gulp-jshint');
 
 gulp.task('javascript', function() {
-	return gulp.src('src/*.js')
+    return gulp.src('src/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter())
-		.pipe(concat('jquery.koha.flipster.js'))
-		.pipe(gulp.dest('dist/'))
-		.pipe(uglify())
-		.pipe(rename({ extname: '-min.js' }))
-		.pipe(gulp.dest('dist/'));
+        .pipe(concat('jquery.koha.flipster.js'))
+        .pipe(gulp.dest('dist/'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '-min.js' }))
+        .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('css', function() {
-	return gulp.src('src/*.css')
-		.pipe(concat('jquery.koha.flipster.css'))
-		.pipe(gulp.dest('dist/'))
-		.pipe(cleancss())
-		.pipe(rename({ extname: '-min.css' }))
-		.pipe(gulp.dest('dist/'));
+    return gulp.src('src/*.css')
+        .pipe(concat('jquery.koha.flipster.css'))
+        .pipe(gulp.dest('dist/'))
+        .pipe(cleancss())
+        .pipe(rename({ extname: '-min.css' }))
+        .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('watch', function() {
-	gulp.watch('src/*.*', ['css','javascript']);
+    gulp.watch('src/*.*', ['css','javascript']);
 });
 
 gulp.task('default', ['css','javascript','watch']);
